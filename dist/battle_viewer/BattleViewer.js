57b23bef90a0024c8068ddb5fb826d8611825883
Artem Bezukladichnyi <overmes@gmail.com>
2025-03-08 17:20:33 +0900

    Mobile friendly.

--- dist/battle_viewer/BattleViewer.js
+++ dist/battle_viewer/BattleViewer.js
@@ -6,26 +6,62 @@ import { BattleGrid } from './components/BattleGrid.js';
 import { ActionControls } from './components/ActionControls.js';
 import { ActionLog } from './components/ActionLog.js';
 import { UnitStatus } from './components/UnitStatus.js';
-import { getOffset } from './utils.js';
 
-const gridStyle = css`
-    width: ${getOffset(GRID_SIZE, 2)}px;
-    height: ${getOffset(GRID_SIZE, 2)}px;`;
+const styles = {
+    container: css`
+        display: flex;
+        flex-direction: row;
+        gap: 1rem;
+        margin: 1rem;
+        background: rgb(249 250 251);
+        max-width: 100%;
+        
+        @media (max-width: 1600px) {
+            flex-direction: column;
+            align-items: center;
+        }
+    `,
+    
+    gridWrapper: css`
+        padding: 1rem;
+        background: rgb(249 250 251);
+        min-width: 700px;
+        min-height: 700px;
+    `,
+    
+    grid: css`
+        width: 100%;
+        aspect-ratio: 1;
+    `,
+    
+    sidePanel: css`
+        padding: 1rem;
+        background: rgb(249 250 251);
+        
+        @media (max-width: 1600px) {
+            width: 100%;
+            max-width: 700px;
+            padding: 0.5rem;
+        }
+    `
+};
 
 export const BattleViewer = ({gameData, myTeam}) => {
     const [currentActionIndex, setCurrentActionIndex] = useState(0);
     const { turns, init_units: initUnits } = gameData;
 
     return html`
-        <div class="flex justify-center m-4 bg-gray-50">
-            <div class="bg-gray-50 p-4 ${gridStyle}">
-                <${BattleGrid}
-                    currentActionIndex=${currentActionIndex}
-                    turns=${turns}
-                    initUnits=${initUnits}
-                />
+        <div class=${styles.container}>
+            <div class=${styles.gridWrapper}>
+                <div class=${styles.grid}>
+                    <${BattleGrid}
+                        currentActionIndex=${currentActionIndex}
+                        turns=${turns}
+                        initUnits=${initUnits}
+                    />
+                </div>
             </div>
-            <div class="p-4 bg-gray-50">
+            <div class=${styles.sidePanel}>
                 ${turns.length > 0 && html`
                     <${ActionControls}
                         currentActionIndex=${currentActionIndex}
@@ -41,7 +77,7 @@ export const BattleViewer = ({gameData, myTeam}) => {
                     actionMap=${gameData.actionMap}
                 />
             </div>
-            <div class="p-4 bg-gray-50">
+            <div class=${styles.sidePanel}>
                 <${UnitStatus}
                     currentActionIndex=${currentActionIndex}
                     turns=${turns}
